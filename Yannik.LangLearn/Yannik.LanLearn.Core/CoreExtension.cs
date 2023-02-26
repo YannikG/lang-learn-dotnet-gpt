3a1e756eef3a29190938d12668041e54934811da
Yannik Gartmann <gartmann.yannik@gmail.com>
2023-02-26 21:39:55 +0000

    add di to core project

---
+++ Yannik.LangLearn/Yannik.LanLearn.Core/CoreExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Yannik.LangLearn.Core.Options;
+using Yannik.LangLearn.Core.Services;
+using Yannik.LangLearn.Core.DataAccess;
+using Yannik.LangLearn.Core.DataAccess.OpenAI;
+using Yannik.LangLearn.Core.DataAccess.Database;
+
+namespace Yannik.LangLearn.Core
+{
+    public static class CoreExtension
+    {
+        public static IServiceCollection ConfigureCore(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.Configure<OpenAIOptions>(
+                configuration.GetSection("OpenAI")
+            );
+
+            service.Configure<MongoDBOptions>(
+                configuration.GetSection("QuestionDatabase")
+            );
+
+            service.AddScoped<OpenAIClientWrapper>();
+            service.AddScoped<MultipleChoiceDatabaseRepository>();
+            service.AddScoped<MultipleChoiceOpenAIRepository>();
+
+            service.AddScoped<MultipleChoiceService>();
+
+            return service;
+        }
+    }
+}
